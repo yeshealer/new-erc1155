dfd1ee6cfa303dc945d4c0b1b3c2c9bd3e952e4b
yeshealer <yeshealer@gmail.com>
2023-09-12 01:40:34 +0900

    fix issue

--- app/drop/page.tsx
+++ app/drop/page.tsx
@@ -1,17 +1,8 @@
 "use client"
 import DropPage from '@/components/pages/DropPage'
-import React, { useEffect, useState } from 'react'
 
 export default function Drop() {
-    const [isReady, setIsReady] = useState(false);
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            setIsReady(true)
-        }
-    }, [])
-
     return (
-        isReady && <DropPage />
+        <DropPage />
     )
 }

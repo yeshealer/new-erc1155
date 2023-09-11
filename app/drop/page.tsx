c1d22920ee295739186864ee03e10884e357199e
yeshealer <yeshealer@gmail.com>
2023-09-12 05:18:13 +0900

    update

--- app/drop/page.tsx
+++ app/drop/page.tsx
@@ -1,16 +1,8 @@
 "use client"
 import DropPage from '@/components/pages/DropPage'
-import { useRouter } from 'next/router'
 
 export default function Drop() {
-    const router = useRouter()
     return (
-        <>
-            {router.isFallback ? (
-                <div>Loading...</div>
-            ) : (
-                <DropPage />
-            )}
-        </>
+        <DropPage />
     )
 }
--- components/widgets/ModelViewer.tsx
+++ components/widgets/ModelViewer.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from 'next/router';
 import '@google/model-viewer/lib/model-viewer';
 
 declare global {
@@ -20,14 +19,7 @@ interface ModelViewerProps {
 export default function ModelViewer({
     prevURL
 }: ModelViewerProps) {
-    const router = useRouter();
     return (
-        <>
-            {router.isFallback ? (
-                <div>Loading</div>
-            ) : (
-                <model-viewer style={{ width: '100%', height: '100%', background: '#e0f2fe', borderRadius: '12px', minHeight: '200px' }} src={prevURL} camera-controls="true" touch-action="pan-y" ar-status="not-presenting" />
-            )}
-        </>
+        <model-viewer style={{ width: '100%', height: '100%', background: '#e0f2fe', borderRadius: '12px', minHeight: '200px' }} src={prevURL} camera-controls="true" touch-action="pan-y" ar-status="not-presenting" />
     )
 }

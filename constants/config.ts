2f37de7b72a87153d44976ef57c1804f2d6121d0
yeshealer <yeshealer@gmail.com>
2023-09-12 06:07:47 +0900

    update

--- constants/config.ts
+++ constants/config.ts
@@ -1,3 +1,3 @@
-export const POLYBASE_BASE_URL = "https://polybase.xyz"
-export const POLYBASE_COLLECTION = "collectiondata00"
-export const POLYBASE_DROP = "dropCollection00"
+export const POLYBASE_BASE_URL = "https://testnet.polybase.xyz"
+export const POLYBASE_COLLECTION = "collectiondata67"
+export const POLYBASE_DROP = "dropCollection38"

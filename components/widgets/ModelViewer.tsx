f2c4ac3be4fbd088096a8034495f5de911ecc1e7
yeshealer <yeshealer@gmail.com>
2023-09-18 20:00:40 +0900

    update model viewer

--- components/widgets/ModelViewer.tsx
+++ components/widgets/ModelViewer.tsx
@@ -1,16 +1,16 @@
 import '@google/model-viewer/lib/model-viewer';
 
-declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            'model-viewer': MyElementAttributes;
-        }
-        interface MyElementAttributes {
-            src: string;
-            style: { width: string; height: string; background: string; borderRadius: string, minHeight: string }
-        }
-    }
-}
+// declare global {
+//     namespace JSX {
+//         interface IntrinsicElements {
+//             'model-viewer': MyElementAttributes;
+//         }
+//         interface MyElementAttributes {
+//             src: string;
+//             style: { width: string; height: string; background: string; borderRadius: string, minHeight: string }
+//         }
+//     }
+// }
 
 interface ModelViewerProps {
     prevURL: string
@@ -19,9 +19,7 @@ interface ModelViewerProps {
 export default function ModelViewer({
     prevURL
 }: ModelViewerProps) {
-    const hasNavigator = typeof navigator !== 'undefined';
-
     return (
-        <model-viewer style={{ width: '99%', height: '100%', background: '#e0f2fe', borderRadius: '12px', minHeight: '200px' }} src={prevURL} camera-controls={hasNavigator ? 'true' : undefined} touch-action="pan-y" ar-status="not-presenting" />
+        <model-viewer style={{ width: '99%', height: '100%', background: '#e0f2fe', borderRadius: '12px', minHeight: '200px' }} src={prevURL} camera-controls="true" touch-action="pan-y" ar-status="not-presenting" />
     )
 }
